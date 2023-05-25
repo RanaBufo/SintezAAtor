c7907d326624c7f98a914ff550830b0bd4ac73f0
RanaBufo <129890115+RanaBufo@users.noreply.github.com>
2023-05-25 22:15:57 +0300

    Add files via upload

---
+++ SiNTeZIK/Sintez/Sintez/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System;
+using System.Speech.Recognition;
+using System.Windows;
+
+namespace WpfTutorialSamples.Audio_and_Video
+{
+    public partial class SpeechRecognitionTextSample : Window
+    {
+        public SpeechRecognitionTextSample()
+        {
+            InitializeComponent();
+            SpeechRecognizer speechRecognizer = new SpeechRecognizer();
+        }
+    }
+}
+        /*
+                private void Speech_Click(object sender, RoutedEventArgs e)
+                {
+                    PromptBuilder builder = new PromptBuilder();
+                    builder.AppendText(t1.Text);
+                    SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+                    speechSynthesizer.Speak("             ");
+                    speechSynthesizer.Speak(builder);
+                }*/
+    
